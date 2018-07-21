00eec13754453aac8dc50681f7848c8af917384a
Yassine <elbayass59@gmail.com>
2018-07-21 23:12:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-	secret: 'montutoautre',
+	secret: 'elbayass',
     saveUninitialized: true,
     resave: true
 }));
