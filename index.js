54c854a07222a4ca3c4dab8e142effd26d94cee8
Yassine <elbayass59@gmail.com>
2018-07-21 23:12:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/tuto', {
+mongoose.connect('mongodb://localhost/elbayass', {
 	useMongoClient: true
 });
 
